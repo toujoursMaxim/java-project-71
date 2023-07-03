8a6619b30c492596c3accefdade440393b760345
Maxim <maxsimus97.ivanov@yandex.ru>
2023-07-03 18:35:00 +0300

    fix App.java, Makefile and add Differ.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,43 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import java.util.concurrent.Callable;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true,
+        description = "Compares two configuration files and shows a difference.")
+
+public final class App implements Callable<Integer> {
+
+    @Parameters(index = "0", description = "path to first file")
+    private String filepath1;
+
+    @Parameters(index = "1", description = "path to second file")
+    private String filepath2;
+
+//    @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
+//    boolean usageHelpRequested;
+//
+//    @Option(names = {"-V", "--version"}, versionHelp = true, description = "Print version information and exit.")
+//    boolean versionInfoRequested;
+
+
+    @Option(names = {"-f", "--format"}, paramLabel = "format",
+            description = "output format [default: stylish]", defaultValue = "stylish")
+    private String format;
+
+    @Override
+    public Integer call() throws Exception {
+        Differ.generate(filepath1, filepath2, format);
+        return null;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        final int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
+
