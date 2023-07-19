63a6d175eb04362a2297edbfba460594c2859d7b
Maxim <maxsimus97.ivanov@yandex.ru>
2023-07-19 23:40:04 +0300

    add Stylish.java

---
+++ app/src/main/java/hexlet/code/formatters/Stylish.java
@@ -0,0 +1,28 @@
+package hexlet.code.formatters;
+
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Stylish {
+    public static String formatStylish(Map <String, Object> file1, Map <String, Object> file2, String format) throws Exception {
+
+        Set<String> keysFromFile = new TreeSet<>(file1.keySet());
+        keysFromFile.addAll(file2.keySet());
+
+        String result = "{\n";
+        for (String key : keysFromFile) {
+            if (file1.containsKey(key) && (!file2.containsKey(key))) {
+                result += String.format("   - " + key + ": " + file1.get(key)) + "\n";
+            } else if (!file1.containsKey(key) && (file2.containsKey((key)))) {
+                result += String.format("   + " + key + ": " + file2.get(key)) + "\n";
+            } else if (!Objects.equals(file1.get(key), file2.get(key))) {
+                result += String.format("   - " + key + ": " + file1.get(key) + "\n" + "   + " + key + ": " + file2.get(key)) + "\n";
+            } else {
+                result += String.format("     " + key + ": " + file1.get(key)) + "\n";
+            }
+        }
+        return result + "}";
+    }
+}
